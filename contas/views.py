6485665dca72b515c6ba2f97460bcc6e43ec8f85
replace-all <thais.vnunes59@gmail.com>
2018-11-08 07:25:50 -0200

    incluindo template

--- contas/views.py
+++ contas/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Transacao
 import datetime
 
 def home(request):
-    
-    now = datetime.datetime.now()
+    data = {}
+    data['clientes'] = ['c1', 'c2', 'c3']
+    data['now'] = datetime.datetime.now()
     #html = "<html><body>It is now %s.</body></html>" % now
     
-    return render(request, 'contas/home.html')
+    return render(request, 'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render (request,'contas/listagem.html', data)
