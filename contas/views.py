3c89983c198ecd864bec1c6e16d1e2276cf59ebd
replace-all <thais.vnunes59@gmail.com>
2018-11-08 06:33:15 -0200

    inclusão das primeiras views, models e urls

---
+++ contas/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+def home(request):
+    
+    now = datetime.datetime.now()
+    #html = "<html><body>It is now %s.</body></html>" % now
+    
+    return render(request, 'contas/home.html')
