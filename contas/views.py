28b5b067204afbf46e796cc825b0c268e2c1416d
replace-all <thais.vnunes59@gmail.com>
2018-11-08 22:20:26 -0200

    implementando o CRUD

--- contas/views.py
+++ contas/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Transacao
+from .form import TransacaoForm
 import datetime
 
 def home(request):
@@ -15,3 +16,24 @@ def listagem(request):
     data = {}
     data['transacoes'] = Transacao.objects.all()
     return render (request,'contas/listagem.html', data)
+
+def nova_transacao(request):
+    form = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    return render (request, 'contas/form.html', {'form':form})
+
+def update(request, pk):
+    form = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    return render (request, 'contas/form.html', {'form':form})
--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from contas.views import home, listagem
+from contas.views import home, listagem, nova_transacao, update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', listagem, name='url_listagem'),
+    path('nova', nova_transacao, name = 'url_nova_transacao'),
+    path('update/<int:pk>', update, name='url_update'),
     path('home', home),
-    path('', listagem),
+
 ]
